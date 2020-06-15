0433bfd125d32b18b3e192f37ba97c97a12c0574
Derrick A Wood <dwood_1994@yahoo.com>
2020-06-15 17:48:00 -0600

    getAll

---
+++ server/controllers/KeepsController.js
@@ -0,0 +1,69 @@
+import express from 'express'
+import BaseController from "../utils/BaseController";
+import auth0provider from "@bcwdev/auth0provider";
+import {
+  keepsService
+} from "../services/KeepsService";
+
+
+
+//PUBLIC
+export class KeepsController extends BaseController {
+  constructor() {
+    super("api/keeps")
+    this.router
+      .use(auth0provider.getAuthorizedUserInfo)
+      .get('', this.getAll)
+      // .get('/:id', this.getById)
+      // .get('/:id/task', this.getTaskByListId)
+      // .post('', this.create)
+      // .put('/:id', this.edit)
+      // .delete('/:id', this.delete)
+  }
+
+
+  async getAll(req, res, next) {
+    try {
+      let data = await keepsService.getAll(req.userInfo.email)
+      return res.send(data)
+    } catch (err) {
+      next(err)
+    }
+  }
+  // async getById(req, res, next) {
+  //   try {
+  //     let data = await keepsService.getById(req.params.id, req.userInfo.email)
+  //     return res.send(data)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // async create(req, res, next) {
+  //   try {
+  //     req.body.creatorEmail = req.userInfo.email
+  //     let data = await keepsService.create(req.body)
+  //     return res.status(201).send(data)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // async edit(req, res, next) {
+  //   try {
+  //     let data = await keepsService.edit(req.params.id, req.userInfo.email, req.body)
+  //     return res.send(data)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
+  // async delete(req, res, next) {
+  //   try {
+  //     await donationsService.delete(req.params.id, req.userInfo.email)
+  //     return res.send("Successfully deleted")
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+}
--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import ValueSchema from "../models/Value";
 import ProfileSchema from "../models/Profile";
-import BoardSchema from '../models/Board'
+import BoardSchema from '../models/Board';
+import KeepSchema from '../models/Keep'
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Profile = mongoose.model("Profile", ProfileSchema);
   Boards = mongoose.model("Board", BoardSchema)
+  Keeps = mongoose.model("Keep", KeepSchema)
 }
 
 export const dbContext = new DbContext();
